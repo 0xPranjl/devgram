f8b68cc0fb205f537994e68deb7c5a8a7f66478a
0xpranjl <pranjal.maurya402@gmail.com>
2023-12-30 20:36:47 +0530

    added contribution graph component

--- src/pages/Gitredirect.js
+++ src/pages/Gitredirect.js
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Bio from '../component/Bio';
+import GitHubCalendar from 'react-github-contribution-calendar';
 const Gitredirect= () => {
   const [name,sname]=useState();
   const [pic,spic]=useState();
   const [rno,srno]=useState();
   const [joindate,sjoindate]=useState();
+  const [uid,suid]=useState();
+
+  var values = {
+    '2016-06-23': 1,
+    '2016-06-26': 2,
+    '2016-06-27': 3,
+    '2016-06-28': 4,
+    '2016-06-29': 4
+  }
+  var until = '2016-06-30';
+
   useEffect(() => {
     const handleCallback = async () => {
-      const code = new URLSearchParams(window.location.search).get('code');
-      console.log(code);
-      const token=await getUserDetails("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");
+      // const code = new URLSearchParams(window.location.search).get('code');
+      // console.log(code);
+      const token=await getUserDetails("ghu_sDDVNMTpaYPyMsbM7xSqT6ODPtyhhj4BtnTx");
       console.log(token);
       //   const response = await axios.post('YOUR_BACKEND_SERVER_URL/github/callback', { code });
     //   console.log(response.data); // Handle the response, store the token, etc.
@@ -28,20 +40,24 @@ const Gitredirect= () => {
     const userDetails = response.data;
     sname(userDetails.name);
     spic(userDetails.avatar_url);
+    suid(userDetails.login);
+    srno(userDetails.public_repos);
+    sjoindate(userDetails.created_at);
     console.log(userDetails);
     return userDetails;
   };
   return (
     <div>
-      <p>Redirecting...</p>
       <div>
       <Bio
         avatar={pic}
-        username={name}
-        totalRepositories={50}
-        dateJoined="January 1, 2022"
+        name={name}
+        username={"( "+uid+" )"}
+        totalRepositories={rno}
+        dateJoined={joindate}
         contributionsThisYear={100}
       />
+      <GitHubCalendar values={values} until={until}/>
     </div>
     </div>
   );
