9904ef5a0ae2c8ba1a4e66a2ff1e44b98102cc59
0xpranjl <pranjal.maurya402@gmail.com>
2023-12-31 13:16:31 +0530

    now private repo also showing but only 30

--- src/pages/Gitredirect.js
+++ src/pages/Gitredirect.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { App } from "octokit";
 import Bio from '../component/Bio';
+import { useSearchParams } from 'react-router-dom';
 import GitHubCalendar from 'react-github-contribution-calendar';
 const Gitredirect= () => {
   const [name,sname]=useState();
@@ -17,35 +19,54 @@ const Gitredirect= () => {
     '2016-06-29': 4
   }
   var until = '2016-06-30';
-
+ const [params]=useSearchParams();
+ console.log(params.get("code"));
+ console.log(params.get("installation_id"));
   useEffect(() => {
     const handleCallback = async () => {
       // const code = new URLSearchParams(window.location.search).get('code');
       // console.log(code);
-      const token=await getUserDetails("ghu_sDDVNMTpaYPyMsbM7xSqT6ODPtyhhj4BtnTx");
-      console.log(token);
+      // const token=await getUserDetails("ghu_ZBWCSQCZdblSulasIY3izSGvyGxs2s1TqmOq");
+      // console.log("token"+token);
+
+    
+
       //   const response = await axios.post('YOUR_BACKEND_SERVER_URL/github/callback', { code });
     //   console.log(response.data); // Handle the response, store the token, etc.
     };
 
     handleCallback();
+  
   }, []);
-  const getUserDetails = async (accessToken) => {
-    const response = await axios.get('https://api.github.com/user', {
+  y("ghu_ygUFFErjh2b5EgQU1aeA6aMLjzrOA60UBeBN");
+  async function y(accessToken){
+    const response = await axios.get('https://api.github.com/user/repos', {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
     });
+    console.log(response);
+  }
+  // const getUserDetails = async (accessToken) => {
+  //   const response = await axios.get('https://api.github.com/user', {
+  //     headers: {
+  //       Authorization: `Bearer ${accessToken}`,
+  //     },
+  //   });
   
-    const userDetails = response.data;
-    sname(userDetails.name);
-    spic(userDetails.avatar_url);
-    suid(userDetails.login);
-    srno(userDetails.public_repos);
-    sjoindate(userDetails.created_at);
-    console.log(userDetails);
-    return userDetails;
-  };
+  //   const userDetails = response.data;
+  //   sname(userDetails.name);
+  //   spic(userDetails.avatar_url);
+  //   suid(userDetails.login);
+  //   srno(userDetails.public_repos);
+  //   sjoindate(userDetails.created_at);
+  //   console.log(userDetails);
+
+
+
+
+  //   return userDetails;
+  // };
   return (
     <div>
       <div>
@@ -62,5 +83,6 @@ const Gitredirect= () => {
     </div>
   );
 };
+
 
 export default Gitredirect;
